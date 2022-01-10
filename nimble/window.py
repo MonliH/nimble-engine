308db89d4081ffc0424bcfce4d70369b23c9702b
Jonathan Li <30177086+MonliH@users.noreply.github.com>
2022-01-09 21:24:40 -0500

    Use advanced docking system

--- nimble/window.py
+++ nimble/window.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import (
     QListView,
     QMainWindow,
 )
+from PyQtAds.QtAds import ads
 
 from nimble.common.shader_manager import Shaders
 
@@ -23,18 +24,18 @@ class MainWindow(QMainWindow):
         self.show()
         self.setWindowTitle("Nimble Engine")
 
-        self.isDockNestingEnabled = True
+        self.dock_manager = ads.CDockManager(self)
 
-        self.viewport_dock = QDockWidget("Scene Viewer")
-        self.addDockWidget(Qt.RightDockWidgetArea, self.viewport_dock)
+        self.viewport_dock = ads.CDockWidget("Scene Viewer")
+        self.dock_manager.addDockWidget(ads.RightDockWidgetArea, self.viewport_dock)
         self.viewport = ViewportWidget(
             self.viewport_dock,
             on_gl_init=self.init_viewport,
         )
         self.viewport_dock.setWidget(self.viewport)
 
-        self.outline_dock = QDockWidget("Outline")
-        self.addDockWidget(Qt.LeftDockWidgetArea, self.outline_dock)
+        self.outline_dock = ads.CDockWidget("Outline")
+        self.dock_manager.addDockWidget(ads.LeftDockWidgetArea, self.outline_dock)
         self.outline = QListView(self.outline_dock)
         self.outline.setModel(active_scene)
         self.outline_dock.setWidget(self.outline)
