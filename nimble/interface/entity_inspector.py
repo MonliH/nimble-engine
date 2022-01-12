fa50b361e6a256aef323e9f2eeb12456782008c8
Jonathan Li <30177086+MonliH@users.noreply.github.com>
2022-01-11 19:15:35 -0500

    link scale and rotation with ui

--- nimble/interface/entity_inspector.py
+++ nimble/interface/entity_inspector.py
@@ -1,18 +1,16 @@
-from typing import Optional, Tuple, cast
-from PyQt5 import QtCore
+import math
+from typing import List, Optional, Tuple, cast
 from PyQt5.QtCore import QSize
 from PyQt5.QtWidgets import (
     QDoubleSpinBox,
-    QHBoxLayout,
     QLabel,
     QLineEdit,
     QWidget,
-    QVBoxLayout,
-    QPushButton,
 )
 from PyQt5 import uic
-from nimble.common.resources import ui_file
+from pyrr.objects.vector3 import Vector3
 
+from nimble.common.resources import ui_file
 from nimble.objects.model import Model, ModelObserver
 from nimble.objects.scene import SceneObserver
 from nimble.objects.scene import active_scene
@@ -28,18 +26,29 @@ class ModelWidget(QWidget, SceneObserver, ModelObserver):
         self.object_name_input = cast(QLineEdit, self.object_name_input)
         self.entity_info = cast(QWidget, self.entity_info)
 
-        self.position_spinners: Tuple[
-            QDoubleSpinBox, QDoubleSpinBox, QDoubleSpinBox
-        ] = (
-            self.x_position,
-            self.y_position,
-            self.z_position,
-        )
+        self.spinners: List[Tuple[QDoubleSpinBox, QDoubleSpinBox, QDoubleSpinBox]] = [
+            (
+                self.x_position,
+                self.y_position,
+                self.z_position,
+            ),
+            (
+                self.x_rotation,
+                self.y_rotation,
+                self.z_rotation,
+            ),
+            (
+                self.x_scale,
+                self.y_scale,
+                self.z_scale,
+            ),
+        ]
 
-        for i, spinner in enumerate(self.position_spinners):
-            spinner.valueChanged.connect(
-                lambda _, i=i: self.position_spinner_changed(i)
-            )
+        for i, spinner_row in enumerate(self.spinners):
+            for j, spinner in enumerate(spinner_row):
+                spinner.valueChanged.connect(
+                    lambda _, i=i, j=j: self.spinner_changed(i, j)
+                )
 
         self.update_view()
         self.object_name_input.textChanged.connect(self.rename_object)
@@ -61,8 +70,10 @@ class ModelWidget(QWidget, SceneObserver, ModelObserver):
             self.object_name_title.setText(f"Object ({self.active.name})")
             self.entity_info.show()
             self.object_name_input.setText(self.active.name)
-            for i, spinner in enumerate(self.position_spinners):
-                spinner.setValue(self.active.position[i])
+            for i, spinner_row in enumerate(self.spinners):
+                for j, spinner in enumerate(spinner_row):
+                    value = self.get_idx(i, self.active)[j]
+                    spinner.setValue(value if i != 1 else math.degrees(value))
         else:
             self.object_name_title.setText(f"No object selected")
             self.entity_info.hide()
@@ -79,8 +90,26 @@ class ModelWidget(QWidget, SceneObserver, ModelObserver):
     def translation_changed(self, obj: Model) -> None:
         self.update_view()
 
-    def position_spinner_changed(self, idx: int):
+    @staticmethod
+    def get_idx(idx: int, obj: Model) -> Vector3:
+        if idx == 0:
+            return obj.position
+        elif idx == 1:
+            return obj.rotation
+        elif idx == 2:
+            return obj.scale
+
+    def spinner_changed(self, row_idx: int, vector_idx: int):
         if self.active is not None:
-            self.active.position[idx] = self.position_spinners[idx].value()
-            self.active.position_changed()
+            new_value = self.spinners[row_idx][vector_idx].value()
+            self.get_idx(row_idx, self.active)[vector_idx] = (
+                new_value if row_idx != 1 else math.radians(new_value)
+            )
+            if row_idx == 0:
+                self.active.position_changed()
+            elif row_idx == 1:
+                self.active.rotation_changed()
+            elif row_idx == 2:
+                self.active.scale_changed()
+
             self.update_view()
