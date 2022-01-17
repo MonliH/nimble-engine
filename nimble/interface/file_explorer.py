eee359d30511cb0a79728dd60c149c951833cad8
Jonathan Li <30177086+MonliH@users.noreply.github.com>
2022-01-17 09:59:36 -0500

    Fix file explorer

--- nimble/interface/file_explorer.py
+++ nimble/interface/file_explorer.py
@@ -1,7 +1,7 @@
 """A virtual file explorer to look at project files."""
 
 from typing import Optional, cast
-from PyQt5.QtWidgets import QTreeView, QWidget
+from PyQt5.QtWidgets import QTreeView, QWidget, QFileSystemModel
 
 from nimble.common.resources import load_ui
 from nimble.objects.project import ProjectObserver, current_project
@@ -10,11 +10,13 @@ from nimble.objects.project import ProjectObserver, current_project
 class FileExplorer(QWidget, ProjectObserver):
     def project_changed(self):
         print(current_project.folder)
-        self.files.setRootIndex(current_project.index(str(current_project.folder)))
+        self.files.setRootIndex(self.model.index(str(current_project.folder)))
 
     def __init__(self, parent: Optional[QWidget] = None):
         super().__init__(parent)
         current_project.add_observer("file_explorer", self)
         load_ui(":/ui/file_explorer.ui", self)
         self.files = cast(QTreeView, self.files)
-        self.files.setModel(current_project)
+        self.model = QFileSystemModel()
+        self.model.setRootPath("")
+        self.files.setModel(self.model)
